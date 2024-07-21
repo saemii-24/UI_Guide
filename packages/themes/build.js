6963c6e4eb5260cccf053f1a3495c9640e74b785
saemii-24 <hims2419@gmail.com>
2024-07-21 13:31:22 +0900

    Init: 리액트 세팅

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -6,15 +6,18 @@ const minify = !dev;
 
 const watch = process.argv.includes("--watch");
 
-const external = Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies });
+const external = Object.keys({
+  ...pkg.dependencies,
+  ...pkg.peerDependencies,
+});
 
 const baseConfig = {
-  entryPoints: ["./src/index.ts"],
+  entryPoints: ["src/index.ts"],
   bundle: true,
-  target: "es2019",
-  sourcemap: true,
-  platform: "node",
   minify,
+  sourcemap: true,
+  outdir: "dist",
+  target: "es2019",
   watch,
   external,
 };
@@ -22,18 +25,16 @@ const baseConfig = {
 Promise.all([
   esbuild.build({
     ...baseConfig,
-    outfile: "./dist/bundle.esm.js", // ESM 버전
     format: "esm",
   }),
   esbuild.build({
     ...baseConfig,
-    outfile: "./dist/bundle.cjs.js", // CommonJS 버전
     format: "cjs",
     outExtension: {
       ".js": ".cjs",
     },
   }),
 ]).catch(() => {
-  console.error("BUILD FAILED");
+  console.error("Build failed");
   process.exit(1);
 });
